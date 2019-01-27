c2649ceb757edd6597a4273f8f424b0031c846d2
NotLebedev <artem-itf@yandex.ru>
2019-01-27 15:15:53 +0300

    Commit status : buildable Test status : all test passed Changes: *Renamed imageRepositoryCreationTest() to creationTest() +Added saveTest() +Added getAllTest() +Added getBetweenTest()

--- src/test/java/ilich/ml/mapserver/DatabaseTests/DatabaseOnMapImageTest.java
+++ src/test/java/ilich/ml/mapserver/DatabaseTests/DatabaseOnMapImageTest.java
@@ -1,9 +1,12 @@
 package ilich.ml.mapserver.DatabaseTests;
 
 import ilich.ml.mapserver.AppConfigTest;
+import ilich.ml.mapserver.Model.Entities.OnMapImageEntity;
 import ilich.ml.mapserver.Model.Repositories.OnMapImageRepository;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 /**
  * @author NotLebedev
@@ -14,12 +17,32 @@ public class DatabaseOnMapImageTest extends AppConfigTest {
     OnMapImageRepository repository;
 
     @Test
-    public void imageRepositoryCreationTest() {
-
+    public void creationTest() {
         assertNotNull(repository);
+    }
+
+    @Test
+    public void saveTest() {
+        for (int i = 0; i < 100; i++) {
+            repository.save(new OnMapImageEntity((long) (-10 * i), (long) (-5 * i), "image" + i + ".jpg"));
+        }
+    }
+
+    @Test
+    public void getAllTest() {
+
+        Iterable<OnMapImageEntity> result = repository.findAll();
+        assertTrue(result.spliterator().getExactSizeIfKnown() > 0);
 
     }
 
+    @Test
+    public void getBetweenTest() {
 
+        List<OnMapImageEntity> result = repository.findBetween(-50L, 0L, -200L, 200L);
+
+        assertEquals(result.size(), 6);
+
+    }
 
 }
