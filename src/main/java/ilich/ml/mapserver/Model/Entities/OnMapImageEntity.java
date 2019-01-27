b1f8ec4586c697603118d45ff020db132093ea67
NotLebedev <artem-itf@yandex.ru>
2019-01-27 14:35:06 +0300

    Commit status : buildable Test status : all test passed Changes: +Added OnMapImageEntity class and declared table structure +Added OnMapImageRepository class

---
+++ src/main/java/ilich/ml/mapserver/Model/Entities/OnMapImageEntity.java
@@ -0,0 +1,37 @@
+package ilich.ml.mapserver.Model.Entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+/**
+ * Database entity representing an image placed on map
+ * @author NotLebedev
+ */
+@Entity
+@Table(
+        name = "ON_MAP_IMAGES",
+        indexes = {
+                @Index(name = "IMAGES_FULL_COORDINATE_INDEX", columnList = "xCoordinate,yCoordinate")
+        })
+public class OnMapImageEntity {
+
+    @Id
+    @GeneratedValue
+    @Getter
+    @Setter
+    private Long id;
+
+    @Getter
+    @Setter
+    private Long xCoordinate;
+    @Getter
+    @Setter
+    private Long yCoordinate;
+
+    @Getter
+    @Setter
+    private String imageUrl;
+
+}
---
+++ src/main/java/ilich/ml/mapserver/Model/Repositories/OnMapImageRepository.java
@@ -0,0 +1,10 @@
+package ilich.ml.mapserver.Model.Repositories;
+
+import ilich.ml.mapserver.Model.Entities.OnMapImageEntity;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author NotLebedev
+ */
+public interface OnMapImageRepository extends CrudRepository<OnMapImageEntity, Long> {
+}
