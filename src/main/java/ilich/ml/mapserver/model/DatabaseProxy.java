82ea7f7f4cb1e3cebc6048586cdc88a1b33ff907
NotLebedev <artem-itf@yandex.ru>
2019-02-02 18:23:16 +0300

    +Declared DatabaseProxy and RequestProxy methods

--- src/main/java/ilich/ml/mapserver/model/DatabaseProxy.java
+++ src/main/java/ilich/ml/mapserver/model/DatabaseProxy.java
@@ -1,5 +1,7 @@
 package ilich.ml.mapserver.model;
 
+import ilich.ml.mapserver.JsonResponseBuilder;
+
 /**
  * @author NotLebedev
  */
@@ -14,6 +16,12 @@ public class DatabaseProxy {
 
     public static DatabaseProxy getInstance() {
         return DatabaseProxyLazyHolder.INSTANCE;
+    }
+
+    public JsonResponseBuilder getImages(JsonResponseBuilder builder, Long centerX, Long centerY, Long width, Long height) {
+
+        return builder;
+
     }
 
 }
--- src/main/java/ilich/ml/mapserver/web/RequestProxy.java
+++ src/main/java/ilich/ml/mapserver/web/RequestProxy.java
@@ -1,9 +1,15 @@
 package ilich.ml.mapserver.web;
 
+import ilich.ml.mapserver.JsonResponseBuilder;
+import ilich.ml.mapserver.model.DatabaseProxy;
+import ilich.ml.mapserver.web.responses.RequestEntitiesJsonResponse;
+
 /**
  * @author NotLebedev
  */
 public class RequestProxy {
+
+    private DatabaseProxy db = DatabaseProxy.getInstance();
 
     private RequestProxy() {
     }
@@ -14,6 +20,14 @@ public class RequestProxy {
 
     public static RequestProxy getInstance() {
         return RequestProxyLazyHolder.INSTANCE;
+    }
+
+    public RequestEntitiesJsonResponse requestEntitiesInView(Long centerX, Long centerY, Long width, Long height) {
+
+        JsonResponseBuilder builder = new JsonResponseBuilder();
+
+        return builder.build();
+
     }
 
 }
