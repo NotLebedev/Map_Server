495eb070b15f785e917778a37cabe922515d486d
NotLebedev <artem-itf@yandex.ru>
2019-02-11 17:52:17 +0300

    +Added zooming *Commented code that spammed on drag

--- src/main/resources/static/scripts/map.js
+++ src/main/resources/static/scripts/map.js
@@ -41,6 +41,32 @@ for(var i = 0; i < entities.length; i++) {
 
 stage.add(layer);
 
-stage.on("dragmove", function () {
-   console.log(stage.x());
+var scaleBy = 1.01;
+stage.on('wheel', e => {
+    e.evt.preventDefault();
+    var oldScale = stage.scaleX();
+
+    var mousePointTo = {
+        x: stage.getPointerPosition().x / oldScale - stage.x() / oldScale,
+        y: stage.getPointerPosition().y / oldScale - stage.y() / oldScale
+    };
+
+    var newScale =
+        e.evt.deltaY > 0 ? oldScale * scaleBy : oldScale / scaleBy;
+    stage.scale({ x: newScale, y: newScale });
+
+    var newPos = {
+        x:
+            -(mousePointTo.x - stage.getPointerPosition().x / newScale) *
+            newScale,
+        y:
+            -(mousePointTo.y - stage.getPointerPosition().y / newScale) *
+            newScale
+    };
+    stage.position(newPos);
+    stage.batchDraw();
 });
+
+/*stage.on("dragmove", function () {
+   console.log(stage.x());
+});*/
