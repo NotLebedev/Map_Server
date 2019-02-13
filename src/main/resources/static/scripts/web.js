d292e065f1b96758517a041bc33651b0c6c1ed14
NotLebedev <artem-itf@yandex.ru>
2019-02-13 19:09:18 +0300

    +Added async requests

--- src/main/resources/static/scripts/web.js
+++ src/main/resources/static/scripts/web.js
@@ -5,11 +5,38 @@ export function httpGet(theUrl) {
     return xmlHttp.responseText;
 }
 
+function httpGetAsync(theUrl, callback) {
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    };
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+}
+
 export function requestEntities(x1, y1, height, width) {
 
     let response = JSON.parse(httpGet(window.location.toString() +
         "/requestEntities?x=" + x1 + "&y=" + y1 + "&height=" + height + "&width=" + width));
 
     return response.responseType.toString() === "RequestEntitiesSuccess" ? response : null;
+
+}
+
+export function requestEntitiesAsync(x1, y1, height, width, callback) {
+
+    let f = function (response) {
+
+        let parse = JSON.parse(response);
+        if(response.responseType.toString() === "RequestEntitiesSuccess") {
+            callback(parse);
+        }
+
+    };
+
+    httpGetAsync(window.location.toString() +
+        "/requestEntities?x=" + x1 + "&y=" + y1 + "&height=" + height + "&width=" + width,
+        f);
 
 }
