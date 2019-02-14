40ce0556e39214971ab059793994f2e28f7cc718
NotLebedev <artem-itf@yandex.ru>
2019-02-14 21:17:39 +0300

    +Added visited positions caching

--- src/main/resources/static/scripts/loadController.js
+++ src/main/resources/static/scripts/loadController.js
@@ -1,36 +1,63 @@
 import {requestEntitiesAsync} from "./web.js";
 
 let loadedEntitiesId = [];
+let visitedPositions = [];
 
 export function loadElementsAsync(x1, y1, width, height, callback) {
 
-    let f = function (response) {
+    const position = new MapPosition(x1, y1, width, height);
 
-        if(response == null) {
+    if(!visitedPositions.some(e => position.equals(e))) {
 
-            console.log("Connectivity issues");
-            return [];
+        let f = function (response) {
 
-        }
+            if (response == null) {
 
-        let entities = response.entities;
-        let ret = [];
+                console.log("Connectivity issues");
+                return [];
 
-        for(let i = 0; i < entities.length; i++) {
-
-            let entity = entities[i];
-
-            if(!loadedEntitiesId.includes(entity.id)) {
-                loadedEntitiesId.push(entity.id);
-                ret.push(entity);
             }
 
-        }
+            visitedPositions.push(position);
 
-        callback(ret);
+            let entities = response.entities;
+            let ret = [];
 
-    };
+            for (let i = 0; i < entities.length; i++) {
 
-    requestEntitiesAsync(Math.round(x1), Math.round(y1), Math.round(height), Math.round(width), f);
+                let entity = entities[i];
+
+                if (!loadedEntitiesId.includes(entity.id)) {
+                    loadedEntitiesId.push(entity.id);
+                    ret.push(entity);
+                }
+
+            }
+
+            callback(ret);
+
+        };
+
+        requestEntitiesAsync(Math.round(x1), Math.round(y1), Math.round(height), Math.round(width), f);
+
+    }
+
+}
+
+class MapPosition {
+
+    constructor(x1, y1, width, height) {
+        this.x1 = Math.floor(x1/100);
+        this.y1 = Math.floor(y1/100);
+        this.width = Math.floor(width/100);
+        this.height = Math.floor(height/100);
+    }
+
+    equals(position) {
+
+        return this.x1 === position.x1 && this.y1 === position.y1 &&
+            this.width === position.width && this.height === position.height;
+
+    }
 
 }
